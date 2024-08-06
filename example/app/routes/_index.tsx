1189eace95b7d6ff86e9ca2ad11db2f1a589c611
rphlmr <raphael@uaerom.com>
2024-08-06 13:58:10 +0200

    make a more complex example

--- example/app/routes/_index.tsx
+++ example/app/routes/_index.tsx
@@ -1,35 +1,36 @@
-import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
-};
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getEnv } from "~/utils/env.server";
 
 export function loader({ context }: LoaderFunctionArgs) {
-  console.log(context);
-  return null;
+  console.log("context", context);
+  return {
+    env: getEnv(),
+  };
 }
 
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs sdsd sdsd
-          </a>
-        </li>
-      </ul>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="mt-8 w-full max-w-4xl overflow-x-auto">
+        <table className="w-full border-collapse bg-gray-100 shadow-md rounded-lg">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Key</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Value</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {Object.entries(data.env).map(([key, value]) => (
+              <tr key={key} className="hover:bg-gray-50">
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{key}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{value ?? "-"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
