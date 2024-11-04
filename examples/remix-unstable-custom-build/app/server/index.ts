6ab411047e5f164ba32d39887fc37ec56bd766f4
rphlmr <raphael@uaerom.com>
2024-11-04 20:08:37 +0100

    📖 DOC: add example

--- examples/remix-unstable-custom-build/app/server/index.ts
+++ examples/remix-unstable-custom-build/app/server/index.ts
@@ -1,5 +1,28 @@
+import { createSecureServer } from "node:http2";
+import mkcert from "mkcert";
 import { createHonoServer } from "react-router-hono-server/node";
+
 import { exampleMiddleware } from "./middleware";
+
+const getSSLConfig = async () => {
+  const ca = await mkcert.createCA({
+    organization: "Development CA",
+    countryCode: "US",
+    state: "California",
+    locality: "San Francisco",
+    validity: 365,
+  });
+
+  const cert = await mkcert.createCert({
+    domains: ["127.0.0.1", "localhost"],
+    validity: 365,
+    ca: { key: ca.key, cert: ca.cert },
+  });
+
+  return { cert: cert.cert, key: cert.key };
+};
+
+const cert = await getSSLConfig();
 
 export const server = await createHonoServer({
   buildDirectory: "dist",
@@ -7,6 +30,13 @@ export const server = await createHonoServer({
     server.use("*", exampleMiddleware());
   },
   listeningListener(info) {
-    console.log(`Server is listening on http://localhost:${info.port}`);
+    console.log(`Server is listening on https://localhost:${info.port}`);
+  },
+  customNodeServer: {
+    createServer: createSecureServer,
+    serverOptions: {
+      cert: cert.cert,
+      key: cert.key,
+    },
   },
 });
