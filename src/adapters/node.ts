c0e4b049e3308dac7c5d12bcc86f51a305e89fd3
rphlmr <raphael@uaerom.com>
2024-12-01 10:57:50 +0100

    handle upgrade listener patch in the code instead of node module patch

--- src/adapters/node.ts
+++ src/adapters/node.ts
@@ -113,23 +113,75 @@ export async function createHonoServer<E extends Env = BlankEnv>(options: HonoSe
     );
     mergedOptions.onServe?.(server);
   } else {
-    const viteDevServer = globalThis.__viteDevServer;
-
     // You wonder why I'm doing this?
     // It is to make the dev server work with `hono/node-ws`
-    if (viteDevServer?.httpServer) {
-      for (const listener of viteDevServer.httpServer.listeners("upgrade")) {
-        if (listener.name !== "hmrServerWsListener") {
-          // @ts-ignore
-          viteDevServer.httpServer.removeListener("upgrade", listener);
-        }
-      }
+    const viteDevServer = globalThis.__viteDevServer;
 
-      mergedOptions.onServe?.(viteDevServer.httpServer);
+    if (!viteDevServer?.httpServer) {
+      return;
     }
+
+    // Remove all user-defined upgrade listeners except HMR
+    cleanUpgradeListeners(viteDevServer.httpServer);
+
+    // Execute your onServe callback
+    mergedOptions.onServe?.(viteDevServer.httpServer);
+
+    // Prevent user-defined upgrade listeners from upgrading `vite-hmr`
+    patchUpgradeListener(viteDevServer.httpServer);
 
     console.log("🚧 Dev server started");
   }
 
   return app;
+}
+
+/**
+ * Clean all user-defined upgrade listeners, except HMR
+ *
+ * Avoid conflicts on `already upgraded connections` when using `@hono/node-ws` on dev
+ *
+ */
+function cleanUpgradeListeners(httpServer: ServerType) {
+  const upgradeListeners = httpServer
+    .listeners("upgrade")
+    .filter((listener) => listener.name !== "hmrServerWsListener");
+
+  for (const listener of upgradeListeners) {
+    httpServer.removeListener(
+      "upgrade",
+      /* @ts-ignore - we don't care */
+      listener
+    );
+  }
+}
+
+/**
+ * Patch all user-defined upgrade listeners, except HMR
+ *
+ * Avoid upgrading `vite-hmr` if `upgrade` listeners are added to the `httpServer` through `onServe` callback
+ *
+ */
+function patchUpgradeListener(httpServer: ServerType) {
+  const upgradeListeners = httpServer
+    .listeners("upgrade")
+    .filter((listener) => listener.name !== "hmrServerWsListener");
+
+  for (const listener of upgradeListeners) {
+    // remove the original listener
+    httpServer.removeListener(
+      "upgrade",
+      /* @ts-ignore - we don't care */
+      listener
+    );
+
+    // re-add the listener back, filtering out `vite-hmr`
+    httpServer.on("upgrade", (request, ...rest) => {
+      if (request.headers["sec-websocket-protocol"] === "vite-hmr") {
+        return;
+      }
+
+      return listener(request, ...rest);
+    });
+  }
 }
