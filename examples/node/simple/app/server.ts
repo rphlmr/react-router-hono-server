236e5cff2900d8542a680f132b9f74adeae2f3b7
rphlmr <raphael@uaerom.com>
2024-12-31 17:28:16 +0100

    example

--- examples/node/simple/app/server.ts
+++ examples/node/simple/app/server.ts
@@ -1,33 +1,5 @@
-import { getConnInfo } from "@hono/node-server/conninfo";
-import { createMiddleware } from "hono/factory";
-import { reactRouterRedirect } from "react-router-hono-server/http";
 import { createHonoServer } from "react-router-hono-server/node";
 
 console.log("loading server");
 
-const localNetwork = createMiddleware(async (c, next) => {
-  const info = getConnInfo(c);
-  console.log("localNetwork", info);
-  console.log(`Remote address is ${info.remote.address}`);
-
-  await next();
-});
-
-const protectRoutes = createMiddleware(async (c, next) => {
-  console.log("Checking", c.req.path);
-
-  if (c.req.path.includes("/protected")) {
-    return reactRouterRedirect("/");
-  }
-
-  return next();
-});
-
-export default await createHonoServer({
-  beforeAll(app) {
-    app.use(protectRoutes);
-  },
-  configure(app) {
-    app.use(localNetwork);
-  },
-});
+export default await createHonoServer();
