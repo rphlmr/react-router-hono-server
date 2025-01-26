e421f711163dbba8b56279b6a7711e69bceb3bfb
rphlmr <raphael@uaerom.com>
2025-01-26 12:09:24 +0100

    fix dev server for deep app dir

---
+++ examples/node/custom-app-dir/vite.config.ts
@@ -0,0 +1,14 @@
+import { reactRouter } from "@react-router/dev/vite";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    reactRouterHonoServer({
+      serverEntryPoint: "src/server/index.ts",
+    }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+});
