c9898a3f365ea6b9c14ba24a741c689a6a84f214
rphlmr <raphael@uaerom.com>
2025-03-07 08:03:35 +0100

    wip

--- examples/vercel/simple/app/entry.server.tsx
+++ examples/vercel/simple/app/entry.server.tsx
@@ -1,63 +1,14 @@
-import { PassThrough } from "node:stream";
-
-import { createReadableStreamFromReadable } from "@react-router/node";
-import { isbot } from "isbot";
-import type { RenderToPipeableStreamOptions } from "react-dom/server";
-import { renderToPipeableStream } from "react-dom/server";
+import { handleRequest } from "@vercel/react-router/entry.server";
 import type { AppLoadContext, EntryContext } from "react-router";
-import { ServerRouter } from "react-router";
 
-export const streamTimeout = 5_000;
-
-export default function handleRequest(
+export default async function (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   routerContext: EntryContext,
-  loadContext: AppLoadContext
-) {
-  return new Promise((resolve, reject) => {
-    let shellRendered = false;
-    let userAgent = request.headers.get("user-agent");
+  loadContext?: AppLoadContext
+): Promise<Response> {
+  const response = await handleRequest(request, responseStatusCode, responseHeaders, routerContext, loadContext);
 
-    // Ensure requests from bots and SPA Mode renders wait for all content to load before responding
-    // https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
-    let readyOption: keyof RenderToPipeableStreamOptions =
-      (userAgent && isbot(userAgent)) || routerContext.isSpaMode ? "onAllReady" : "onShellReady";
-
-    const { pipe, abort } = renderToPipeableStream(<ServerRouter context={routerContext} url={request.url} />, {
-      [readyOption]() {
-        shellRendered = true;
-        const body = new PassThrough();
-        const stream = createReadableStreamFromReadable(body);
-
-        responseHeaders.set("Content-Type", "text/html");
-
-        resolve(
-          new Response(stream, {
-            headers: responseHeaders,
-            status: responseStatusCode,
-          })
-        );
-
-        pipe(body);
-      },
-      onShellError(error: unknown) {
-        reject(error);
-      },
-      onError(error: unknown) {
-        responseStatusCode = 500;
-        // Log streaming rendering errors from inside the shell.  Don't log
-        // errors encountered during initial shell rendering since they'll
-        // reject and get logged in handleDocumentRequest.
-        if (shellRendered) {
-          console.error(error);
-        }
-      },
-    });
-
-    // Abort the rendering stream after the `streamTimeout` so it has tine to
-    // flush down the rejected boundaries
-    setTimeout(abort, streamTimeout + 1000);
-  });
+  return response;
 }
