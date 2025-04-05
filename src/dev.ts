66ee9e4931fcf3657561c00f94cb38545d44ee33
rphlmr <raphael@uaerom.com>
2025-04-05 20:54:15 +0200

    fix regex

--- src/dev.ts
+++ src/dev.ts
@@ -275,12 +275,20 @@ export function reactRouterHonoServer(options: ReactRouterHonoServerPluginOption
           new RegExp(
             `^(?=\\/${pluginConfig.appDirectory.replace(/^[/\\]+|[/\\]+$/g, "").replaceAll(/[/\\]+/g, "/")}\\/)((?!.*\\.data(\\?|$)).*\\..*(\\?.*)?$)`
           ),
-          new RegExp(`^(?=\\/${pluginConfig.appDirectory.split("/")[0]}\\/)((?!.*\\.data(\\?|$)).*\\..*(\\?.*)?$)`),
+          new RegExp(
+            `^(?=\\/${pluginConfig.appDirectory
+              .split("/")[0]
+              .replace(/^[/\\]+|[/\\]+$/g, "")
+              .replaceAll(/[/\\]+/g, "/")}\\/)((?!.*\\.data(\\?|$)).*\\..*(\\?.*)?$)`
+          ),
           /\?import(\?.*)?$/,
           /^\/@.+$/,
           /^\/node_modules\/.*/,
           `^(?=\\/${pluginConfig.appDirectory.replace(/^[/\\]+|[/\\]+$/g, "").replace(/[/\\]+/g, "/")}/**/.*/**)`,
-          `^(?=\\/${pluginConfig.appDirectory.split("/")[0]}/**/.*/**)`,
+          `^(?=\\/${pluginConfig.appDirectory
+            .split("/")[0]
+            .replace(/^[/\\]+|[/\\]+$/g, "")
+            .replace(/[/\\]+/g, "/")}/**/.*/**)`,
           ...(pluginConfig.dev?.exclude || []),
         ],
       });
