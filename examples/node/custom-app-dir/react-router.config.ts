b4ae519d62e78736f248556894a8b0dfa33b52ae
rphlmr <raphael@uaerom.com>
2025-08-05 23:23:12 +0200

    lint

--- examples/node/custom-app-dir/react-router.config.ts
+++ examples/node/custom-app-dir/react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "src/app",
   future: {
-		unstable_optimizeDeps: true,
-		unstable_splitRouteModules: true,
-	},
+    unstable_optimizeDeps: true,
+    unstable_splitRouteModules: true,
+  },
 } satisfies Config;
--- examples/node/custom-app-dir/src/server/index.ts
+++ examples/node/custom-app-dir/src/server/index.ts
@@ -5,7 +5,7 @@ console.log("loading server");
 
 export default await createHonoServer({
   getLoadContext(c, { mode, build }) {
-    console.log('from helper', someExternalHelper())
+    console.log("from helper", someExternalHelper());
     const isProductionMode = mode === "production";
     return {
       appVersion: isProductionMode ? build.assets.version : "dev",
