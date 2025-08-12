ee2abf2f618856cbb493c109388b253729c2e555
Raphaël Moreau <20722140+rphlmr@users.noreply.github.com>
2025-08-12 09:47:59 +0200

    Feat/react router 7.8.0 (#155)
    * Add support for React Router 7.8.0 unstable middleware changes
    
    * Add provenance flag to publishConfig in package.json

--- examples/node/simple-future-middleware/app/server.ts
+++ examples/node/simple-future-middleware/app/server.ts
@@ -1,4 +1,4 @@
-import { unstable_createContext } from "react-router";
+import { unstable_RouterContextProvider, unstable_createContext } from "react-router";
 import { createHonoServer } from "react-router-hono-server/node";
 
 console.log("loading server");
@@ -10,8 +10,12 @@ type GlobalAppContext = {
 export const globalAppContext = unstable_createContext<GlobalAppContext>();
 
 export default await createHonoServer({
-  getLoadContext(c, { mode, build }) {
+  getLoadContext(_c, { mode, build }) {
     const isProductionMode = mode === "production";
-    return new Map([[globalAppContext, { appVersion: isProductionMode ? build.assets.version : "dev" }]]);
+    const context = new unstable_RouterContextProvider();
+
+    context.set(globalAppContext, { appVersion: isProductionMode ? build.assets.version : "dev" });
+
+    return context;
   },
 });
