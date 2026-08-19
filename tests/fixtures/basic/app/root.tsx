f4364141732d99ac982bac89792c96aae336c603
rphlmr <raphael@uaerom.com>
2026-08-19 14:09:56 +0200

    ci: 🎡 use system Chrome in CI

--- tests/fixtures/basic/app/root.tsx
+++ tests/fixtures/basic/app/root.tsx
@@ -9,6 +9,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="data:," />
         <Meta />
         <Links />
       </head>
